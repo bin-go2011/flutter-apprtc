515739a745194bb24a17239c9c9d95d035587afe
ben <bin.go2011@yahoo.com>
2021-05-01 23:42:31 +0800

    ui is almost done.

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,67 @@ class HomePage extends StatelessWidget {
         title: const Text('AppRTC Demo'),
       ),
       backgroundColor: Color(0xff333333),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 50,
+                left: 25,
+                bottom: 5,
+              ),
+              child: Text(
+                'Please enter a room name',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+            ),
+            child: TextField(
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 120,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('JOIN'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                    )),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              SizedBox(
+                width: 120,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('RANDOM'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                    )),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
