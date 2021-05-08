39ac3b7a441284b76dcd5e216c6b4186cbb3a727
ben <bin.go2011@yahoo.com>
2021-05-09 00:40:19 +0800

    _sendSignalingMessage

--- lib/src/call.dart
+++ lib/src/call.dart
@@ -3,13 +3,17 @@ import 'dart:convert';
 import 'package:flutter_apprtc/src/signaling_channel.dart';
 import 'package:http/http.dart' as http;
 
+const APPRTC_URL_BASE = 'https://appr.tc';
+
 class Call {
   SignalingChannel _channel;
   Map<String, Object> _params;
 
+  String _roomId;
+  String _clientId;
+
   Call(this._params) {
     _channel = SignalingChannel(_params["wssUrl"]);
-    _requestMediaAndIceServers();
   }
 
   _requestMediaAndIceServers() {
@@ -30,27 +34,44 @@ class Call {
     print('Response body: ${response.body}');
   }
 
-  start(String roomId) {
-    _connectToRoom(roomId);
+  start(String roomId) async {
+    _roomId = roomId;
+
+    _requestMediaAndIceServers();
+
+    await _connectToRoom();
+
+    _startSignaling();
   }
 
-  _connectToRoom(String roomId) async {
+  _connectToRoom() async {
     _channel.open();
 
-    var response = await _joinRoom(roomId);
+    var response = await _joinRoom(_roomId);
 
     if (response.statusCode == 200) {
       var responseObj = jsonDecode(response.body);
       if (responseObj["result"] == "SUCCESS") {
-        var clientId = responseObj["params"]["client_id"];
-        _channel.register(roomId, clientId);
+        _clientId = responseObj["params"]["client_id"];
+        _channel.register(_roomId, _clientId);
       }
     }
     print('Joined the room.');
   }
 
   Future<http.Response> _joinRoom(String roomId) {
-    var url = Uri.parse('https://appr.tc/join/$roomId');
+    var url = Uri.parse('$APPRTC_URL_BASE/join/$roomId');
     return http.post(url);
+  }
+
+  _startSignaling() {
+    print('Starting signaling.');
+  }
+
+  _sendSignalingMessage(dynamic message) {
+    var msgString = jsonEncode(message);
+    var url = Uri.parse('$APPRTC_URL_BASE/message/$_roomId/$_clientId');
+    http.post(url, body: msgString);
+    print('C->GAE: $msgString');
   }
 }
